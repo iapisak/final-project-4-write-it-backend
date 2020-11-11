7a3b4fa3b8907acc00e874c9de0250eccc5561cb
iapisak <apotisanmetee@gmail.com>
2020-11-11 12:32:36 -0800

    remove access allow

--- server.js
+++ server.js
@@ -15,14 +15,7 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 
-app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://project-write-it.herokuapp.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-  })
+app.use(cors(corsOptions));
 
 // === MiddleWare === //
 app.use(bodyParser.json());
