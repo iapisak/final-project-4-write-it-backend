5e5b6176e280ebec76d5f9b9ec0c9282cf4c8b8c
iapisak <apotisanmetee@gmail.com>
2020-11-12 12:10:05 -0800

    update

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ const routes = require('./routes/api');
 
 const corsOptions = {
     "Access-Control-Allow-Origin": ['http://localhost:3000', 'https://project-write-it.herokuapp.com'],
-    "Access-Control-Allow-Methods": POST,GET,PUT,DELETE,
     "Access-Control-Allow-Credentials": true,
-    // Origin: ['http://localhost:3000', 'https://project-write-it.herokuapp.com'],
     Credentials: true,
     optionsSuccessStatus: 200
 }
