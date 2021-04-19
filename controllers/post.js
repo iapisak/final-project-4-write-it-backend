0ac8f5d547540352435257b4dc4243a900db6591
iapisak <apotisanmetee@gmail.com>
2021-04-19 15:58:56 -0700

    clear error

--- controllers/post.js
+++ controllers/post.js
@@ -53,12 +53,12 @@ const updatePosts = async (req, res) => {
 // Delete Post //
 const deletePost = async (req, res) => {
     try {
-        const deletePost = await db.Post.findByIdAndDelete(req.params.post_Id)
         const deleteComment = await db.Comment.find({ post: req.params.post_Id }).populate("post")
         deleteComment.map( async item => {
             const result = await db.Comment.deleteMany(item)
             return result
         })
+        const deletePost = await db.Post.findByIdAndDelete(req.params.post_Id)
         res.json({ status: 200, post: deletePost, comment: deleteComment })
     } catch (err) {
         return res.status(500).json({ error: "Could not find this post"})
